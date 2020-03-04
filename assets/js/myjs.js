abe6906b1afff1d17df4275e0e6ee3c2d2d13086
MunirSaffur <47296924+MunirSaffur@users.noreply.github.com>
2020-03-04 14:06:28 +0300

    uplading project

---
+++ assets/js/myjs.js
@@ -0,0 +1,12 @@
+$(window).on('load', function(){
+    $("#commonsidebar").load("sidebar.html");
+  });
+/* let ul = document.querySelector("ul");
+let li = document.querySelector("li");
+
+li.forEach(el => {
+  el.addEventListener('click',function(){
+    ul.querySelector('.active').classList.remove('active');
+    el.classList.add('active');
+  });
+}); */
