e62f73a2011fa8f6d6ffd2af2e22760962667203
hayarafee <hayaaaarafee@gmail.com>
2020-03-04 17:30:51 +0300

    active list update

--- assets/js/myjs.js
+++ assets/js/myjs.js
@@ -1,12 +1,7 @@
-$(window).on('load', function(){
-    $("#commonsidebar").load("sidebar.html");
-  });
-/* let ul = document.querySelector("ul");
-let li = document.querySelector("li");
 
-li.forEach(el => {
-  el.addEventListener('click',function(){
-    ul.querySelector('.active').classList.remove('active');
-    el.classList.add('active');
-  });
-}); */
+
+$(window).on('load', function(){
+  $("#commonsidebar").load("sidebar.html");
+
+});
+
