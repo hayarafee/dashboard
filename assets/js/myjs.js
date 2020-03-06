ca516cb4aa618a64711bcab2b5f550af7d1761aa
hayarafee <hayaaaarafee@gmail.com>
2020-03-06 17:38:03 +0300

    add new order update

--- assets/js/myjs.js
+++ assets/js/myjs.js
@@ -1,7 +1,20 @@
+const addNewOrder = document.getElementById("addOrder");
+const closepopUp = document.getElementById("closepop");
 
+eventsList();
+function eventsList() {
+  addNewOrder.addEventListener("click", openpopup);
+    closepopUp.addEventListener("click", closePoup);
+  }
+
+  function openpopup(){
+    popupBox.style.display = "block";
+}
+function closePoup(){
+    popupBox.style.display = "none";
+}
 
 $(window).on('load', function(){
   $("#commonsidebar").load("sidebar.html");
 
 });
-
